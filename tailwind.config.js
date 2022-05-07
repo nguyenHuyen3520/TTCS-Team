2363d268f204a570b69b0297774c20986c34f495
Nguyễn Văn Huyên <61755848+nguyenHuyen3520@users.noreply.github.com>
2022-05-07 07:21:01 +0700

    l

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'default': '#444791',
+        'bg-hover' : '#f5f5f5',
+        'dark_mode': '#0a0a0a',
+        'bg-dark_mode': '#1b1a1a',
+        'bg-light': '#d4d5db'
+      },
+    },
+  },
+  plugins: [],
+}
